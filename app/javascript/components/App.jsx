ceccf976125a4a65dd73bdbdc6155887bcd232af
Finn Francis <finnfrancis123@gmail.com>
2020-04-02 10:41:31 +0100

    172103985 - Added an admin area
    Why?
    - Gives us a separate area for admins to manage the app from.

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,21 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Home from "../components/Home";
+import Admin from "../components/Admin";
+import AdminExercises from '../components/admin/exercises/Index'
 
 class App extends React.Component {
   render() {
     return (
       <div className='container'>
-        <nav>
-          <h1>Our Fitness Pal</h1>
-        </nav>
         <Router>
+          <nav>
+            <Link to="/" className="btn btn-lg custom-button" role="button"><h1>Our Fitness Pal</h1></Link>
+          </nav>
           <Switch>
             <Route path="/" exact component={Home} />
-            {/* <Route path="/workouts" exact component={Workouts} /> */}
+            <Route path="/admin" exact component={Admin} />
+            <Route path="/admin/exercises" exact component={AdminExercises} />
           </Switch>
+          <footer></footer>
         </Router>
-        <footer></footer>
       </div>
     )
   }
