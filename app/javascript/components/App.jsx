9f4ca7629d14dfa3a0c50034dc3e99feffb929cc
Finn Francis <finnfrancis123@gmail.com>
2020-04-02 09:27:36 +0100

    Moved all of the routing to the app component
    Why?
    - This allows us to put a layout around each page

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,4 +1,24 @@
 import React from "react";
-import Routes from "../routes/Index";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "../components/Home";
 
-export default props => <>{Routes}</>;
+class App extends React.Component {
+  render() {
+    return (
+      <div className='container'>
+        <nav>
+          <h1>Our Fitness Pal</h1>
+        </nav>
+        <Router>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            {/* <Route path="/workouts" exact component={Workouts} /> */}
+          </Switch>
+        </Router>
+        <footer></footer>
+      </div>
+    )
+  }
+}
+
+export default App
