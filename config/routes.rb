ad1f8d4ee3ae932c2de81c3f681444ba01c4215f
Finn Francis <finnfrancis123@gmail.com>
2020-04-02 17:01:18 +0100

    172126170 - Configured devise sessions to work with jwt and json
    How?
    - Created a new sessions controller which inherits from the devise sessions controller but overrides some methods/settings.
    - Changed the sessions routes to point to the new sessions controller.
    - Removed the current_user method from ApplictionController as it's no longer needed.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+             path: '',
+             path_names: {
+               sign_in: 'login',
+               sign_out: 'logout'
+             },
+             controllers: {
+               sessions: 'api/v1/sessions'
+             }
   root 'homepage#index'
   get '/*path' => 'homepage#index'
 end
