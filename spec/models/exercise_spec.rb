bcaa04c946ad3e01f010f6850de05f9ccd78d5f1
Ben Hornsby <ben.hornsby@pharmaseal.co>
2020-04-02 15:06:06 +0100

    Implements Exercise Model
    Why?
    - Need to be able to add exercises to the system to be used for creating workouts
    
    How?
    - Adds model, migration, model spec and factory

---
+++ spec/models/exercise_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Exercise, type: :model do
+  it 'has a valid factory' do
+    expect(build(:exercise)).to be_valid
+  end
+
+  describe 'attributes' do
+    it { is_expected.to(have_attribute(:name)) }
+    it { is_expected.to(have_attribute(:description)) }
+  end
+
+  describe 'required attributes' do
+    it { should validate_presence_of(:name).with_message('can\'t be blank') }
+  end
+end
