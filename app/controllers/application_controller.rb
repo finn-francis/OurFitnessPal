ec789f6daf57baf84e17ea425bf5bc87fc7a9400
Finn Francis <finnfrancis123@gmail.com>
2020-04-02 17:11:28 +0100

    172126170 - Made the devise registrations controller work for jwt and json
    How?
    - Created a new RegistrationsController which inherits from Devise::RegistrationsController and overrides key settings and methods.
    - Changed the devse routes to use the new registrations controller.
    - Added a method in ApplicationController for rendering json or an error.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,22 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  protected
+
+  def render_resource(resource)
+    if resource.errors.empty?
+      render json: resource
+    else
+      render json: {
+        errors: [
+          {
+            status: '400',
+            title: 'Bad Request',
+            detail: resource.errors,
+            code: '100'
+          }
+        ]
+      }, status: :bad_request
+    end
+  end
 end
