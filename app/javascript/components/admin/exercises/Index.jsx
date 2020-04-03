d54f76386ed3491ea9bd6e76a438521291ef4c74
Ben Hornsby <ben.hornsby@pharmaseal.co>
2020-04-03 09:58:47 +0100

    Builds out index component
    - Adds fetch request to component did mount to receive exercise data.
    - Renders out the data as a list

--- app/javascript/components/admin/exercises/Index.jsx
+++ app/javascript/components/admin/exercises/Index.jsx
@@ -1,5 +1,56 @@
 import React from 'react';
 
-export default () => (
-  <h2>Exercises</h2>
-)
+class Index extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      exercises: []
+    };
+  }
+
+  componentDidMount() {
+    const url = "/api/v1/exercises";
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then(response => this.setState({ exercises: response }));
+  }
+
+  render() {
+    const { exercises } = this.state;
+    const allExercises = exercises.map((exercise, index) => (
+      <div key={index} className="col-md-6 col-lg-4">
+        <div className="card mb-4">
+          <div className="card-body">
+            <h5 className="card-title">{exercise.name}</h5>
+          </div>
+        </div>
+      </div>
+    ));
+    const noExercises = (
+      <div className="vw-100 vh-50 d-flex align-items-center justify-content-center">
+        <h4>
+          No exercises
+        </h4>
+      </div>
+    );
+
+    return (
+      <>
+        <div className="py-5">
+          <main className="container">
+            <div className="row">
+              {exercises.length > 0 ? allExercises : noExercises}
+            </div>
+          </main>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Index;
