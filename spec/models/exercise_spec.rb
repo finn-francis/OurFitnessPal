4d33d736f8679009cd61271f29bfdd3ffd42df71
Ben Hornsby <ben.hornsby@pharmaseal.co>
2020-04-06 17:14:44 +0100

    Adds uniqueness validator to name

--- spec/models/exercise_spec.rb
+++ spec/models/exercise_spec.rb
@@ -14,5 +14,6 @@ describe Exercise, type: :model do
 
   describe 'required attributes' do
     it { should validate_presence_of(:name).with_message('can\'t be blank') }
+    it { should validate_uniqueness_of(:name).with_message('has already been taken') }
   end
 end
